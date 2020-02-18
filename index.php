f230682f8817e9f7f91b5ce10ddc1f8ef8d8795c
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-02-18 11:40:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title> Test </title>
+	</head>
+	<body>
+  <a href = https://api-tan-endterm.herokuapp.com/form_create.php> Create </a> <br>
+  <a href = https://api-tan-endterm.herokuapp.com/form_delete.php> Delete </a> <br>
+  </body>
+  </html>
