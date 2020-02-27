0ac4607f113da8d52a08199d809cfce855c2d2bd
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-02-27 11:22:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 	<body>
 	  <a href = https://api-tan-endterm.herokuapp.com/form_create.php> Create </a> &nbsp;
 	  <a href = https://api-tan-endterm.herokuapp.com/form_delete.php> Delete </a> &nbsp;
-	  <a href = https://api-tan-endterm.herokuapp.com/list_products.php> Read </a> &nbsp;
+	  <a href = https://api-tan-endterm.herokuapp.com/product_list.php> Read </a> &nbsp;
 	 </body>
   </html>
