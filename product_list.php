c4e118ba732a3fca21fac4ed902a4fdb307cb9d1
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-03 11:00:41 +0800

    Update product_list.php

--- product_list.php
+++ product_list.php
@@ -35,7 +35,7 @@
 foreach($list as $value){
     ?>
     <tr>
-        <td><a href = "index.php?page=Details$id=<?php echo $value ['id'];?>"><?php echo $value['name'];?></a></td>
+        <td><a href = "index.php?page=Details&id=<?php echo $value ['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['description'];?></td>
         <td><?php echo $value['price'];?></td>
         <td><?php echo $value['category_name'];?></td>
