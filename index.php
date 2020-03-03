2ae5f5dd13b536cc37807409f1451d0851b0368b
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-03 11:15:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,28 +8,4 @@
 	  <a href = https://api-tan-endterm.herokuapp.com/product_list.php> Read </a> &nbsp;
 	  <a href = https://api-tan-endterm.herokuapp.com/category_list.php> Categories </a> &nbsp;
 	 </body>
-		<?php 
-			switch($page){
-				case 'Product':
-					require_once 'product_list.php';
-				break;
-				case 'Category':
-					require_once 'category_list.php';
-				break;
-				case 'AddProd':
-					require_once 'form_create.php';
-				break;
-				case 'Details':
-					require_once 'product_info.php';
-				break;
-				case 'Update':
-					require_once 'product_update.php';
-				break;
-				case 'Delete':
-					require_once 'product_delete.php';
-				break;	
-				default:
-				break;
-			}
-			?>
   </html>
