fcdac1fbcfea4068ea0272b6833cb0555b628f24
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-03 10:50:25 +0800

    Update product_list.php

--- product_list.php
+++ product_list.php
@@ -19,7 +19,7 @@ $search = (isset($_POST['search']) && $_POST['search'] != '') ? $_POST['search']
 
 <h1>Products</h1>
 
-<form method = "POST" action="product_list.php">
+<form method = "POST" action="index.php?page=Product">
 	<input class="search" type="text" name ="search">
 	<input class="submit" type="submit" name="submit" value="SEARCH">
 </form>
