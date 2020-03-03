d4e4265e23ab3fd60418bf73e8416d5eb5b66fad
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-03 11:30:49 +0800

    Update product_delete.php

--- product_delete.php
+++ product_delete.php
@@ -9,16 +9,15 @@
 <html>
 	<head> 
 		<title> Delete Confirm </title>
-		<link rel="stylesheet" type="text/css" href="css/styles_supplier.css">
 	</head>
 	<body>
 		<h1>Are you sure to delete <?php echo $result['name']?>?</h1>
 		<br>
 		<form action = "pro_delete.php?id=<?php echo $id?>" method = "POST">
-			<input class="delete" type="Submit" name="Submit" value="Yes">
+			<input type="Submit" name="Submit" value="Yes">
 		</form>
 		<form action="product_list.php">
-			<button class="delete" type="submit"> No </button>
+			<input type="Submit" name="No">
 		</form>
 	</body>
 </html>
