cc2387d7b9bf0f3dced7a76162d5ed8ebc35ae47
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-03 10:51:16 +0800

    Update product_delete.php

--- product_delete.php
+++ product_delete.php
@@ -19,7 +19,7 @@
 		<form action = "pro_delete.php?id=<?php echo $id?>" method = "POST">
 			<input class="delete" type="Submit" name="Submit" value="Yes">
 		</form>
-		<form action="index.php?">
+		<form action="index.php?page=Product">
 			<button class="delete" type="submit"> No </button>
 		</form>
 	</body>
