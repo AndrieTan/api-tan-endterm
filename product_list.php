ef1adccc2be91d13ed5a229184a54c5495b9e335
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-05 11:00:14 +0800

    Update product_list.php

--- product_list.php
+++ product_list.php
@@ -17,7 +17,7 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">    
 <center><h1>PRODUCTS</h1>
 
-<form method = "POST" action="product_list.php">
+<form method = "POST" action="index.php?page=product">
 	<input class="search" type="text" name ="search">
 	<input class="submit" type="submit" name="submit" value="SEARCH">
 </form>
@@ -33,7 +33,7 @@
 foreach($list as $value){
     ?>
     <tr>
-        <td><a href = "product_info.php?id=<?php echo $value ['id'];?>"><?php echo $value['name'];?></a></td>
+        <td><a href = "index.php?page=info&id=<?php echo $value ['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['description'];?></td>
         <td><?php echo $value['price'];?></td>
         <td><?php echo $value['category_name'];?></td>
