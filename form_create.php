2eddce17a81f99bd68627a61a3a53e411e3751c5
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-05 10:28:17 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -1,11 +1,17 @@
-<title> API Inventory </title>
-<link rel="stylesheet" type="text/css" href="css/style.css">    
-<form action="pro_create.php" method="POST">
-	<center><h1> ADD PRODUCT </h1>
-	<hr style ="width: 25%;">
-		<input type="text" name="name" placeholder="name"/><br>
-		<input type="text" name="description" placeholder="description"/><br>
-		<input type="text" name="price" placeholder="price"/><br>
-		<input type="text" name="category" placeholder="category"/><br><br>
-		<input type="submit" name="submit" value="SUBMIT"/>
-</form>
+<html>
+	<head> 
+		<title> API Inventory </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<form action="pro_create.php" method="POST">
+			<center><h1> ADD PRODUCT </h1>
+			<hr style ="width: 25%;">
+				<input type="text" name="name" placeholder="name"/><br>
+				<input type="text" name="description" placeholder="description"/><br>
+				<input type="text" name="price" placeholder="price"/><br>
+				<input type="text" name="category" placeholder="category"/><br><br>
+				<input type="submit" name="submit" value="SUBMIT"/>
+		</form>
+	</body>
+</html>
