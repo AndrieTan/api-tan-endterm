727527c9e66fb6351603d4efce6f561f218193d3
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-05 09:20:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
 	<head>
 		<title> Test </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">   
 	</head>
 	<body>
-	  <a href = https://api-tan-endterm.herokuapp.com/form_create.php> Create </a> &nbsp;
-	  <a href = https://api-tan-endterm.herokuapp.com/product_list.php> Read </a> &nbsp;
-	  <a href = https://api-tan-endterm.herokuapp.com/category_list.php> Categories </a> &nbsp;
-	 </body>
-  </html>
+		<a href = https://api-tan-endterm.herokuapp.com/form_create.php> Create </a> &nbsp;
+		<a href = https://api-tan-endterm.herokuapp.com/product_list.php> Read </a> &nbsp;
+	  	<a href = https://api-tan-endterm.herokuapp.com/category_list.php> Categories </a> &nbsp;
+	 </body
+</html>
