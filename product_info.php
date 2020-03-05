1615e79561ce6d76ed8715067f3c064c5210832a
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-05 10:04:50 +0800

    Update product_info.php

--- product_info.php
+++ product_info.php
@@ -10,15 +10,10 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">    
 	<center><h1> PRODUCT DETAILS </h1>
 	<hr style ="width: 25%;">
-	<br>
-		<h2>Name: </h2>
-		<h2><?php echo $result['name']; ?></h2>
-		<h2>Description: </h2>
-		<h2><?php echo $result['description']; ?></h2>
-		<h2>Price: </h2>
-		<h2><?php echo $result['price']; ?></h2>
-		<h2>Category: </h2>
-		<h2><?php echo $result['category_name']; ?></h2>
+		<h2>Name: </h2><h2><?php echo $result['name']; ?></h2>
+		<h2>Description: </h2><h2><?php echo $result['description']; ?></h2>
+		<h2>Price: </h2><h2><?php echo $result['price']; ?></h2>
+		<h2>Category: </h2><h2><?php echo $result['category_name']; ?></h2>
 		<br>
 <a href="product_update.php?id=<?php echo $id; ?>" type="submit">UPDATE</a>&nbsp;
 <a href="product_delete.php?id=<?php echo $id; ?>" type="submit">DELETE</a>
