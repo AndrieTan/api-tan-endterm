984de125070697d830464eea4c2d68e402aa784a
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-03-05 10:31:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 		<title> API Inventory </title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">   
 	</head>
+	<h1 style = "font-size:21px"; "text-align:center";> API INVENTORY </h1>
 	<body>
 	   <center>
 		<a href = https://api-tan-endterm.herokuapp.com/form_create.php> Create </a> &nbsp;
